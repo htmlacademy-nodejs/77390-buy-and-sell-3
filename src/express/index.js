a8b55168ccccccfc05c054ddebc9dd0811c3a4d2
A.Alekseev <angelpsymail@gmail.com>
2020-04-11 21:16:03 +0500

    add templates

--- src/express/index.js
+++ src/express/index.js
@@ -1,19 +1,31 @@
 'use strict';
 
 require(`../utils/env`);
-const colors = require(`colors/safe`);
 const express = require(`express`);
+const path = require(`path`);
+const colors = require(`colors/safe`);
+
 const offersRoutes = require(`./routes/offers`);
 const myRoutes = require(`./routes/my`);
 const mainRoutes = require(`./routes/main`);
 
 const PORT = process.env.DEFAULT_PORT_FRONT_SERVER;
+const PUBLIC_DIR = `public`;
 
 const app = express();
+
+app.set(`views`, path.resolve(__dirname, `templates`, `pages`));
+app.set(`view engine`, `pug`);
 
 app.use(`/offers`, offersRoutes);
 app.use(`/my`, myRoutes);
 app.use(`/`, mainRoutes);
+
+app.use(express.static(path.resolve(__dirname, PUBLIC_DIR)));
+
+app.use((req, res) => {
+  res.status(404).render(`errors/400`);
+});
 
 app.listen(PORT, () => {
   console.log(colors.green(`Ожидаю соединений на ${PORT}`))
--- src/express/routes/offers.js
+++ src/express/routes/offers.js
@@ -3,9 +3,9 @@
 const {Router} = require(`express`);
 const router = new Router();
 
-router.get(`/category/:id`, (req, res) => res.send(`/offers/category/:id`));
-router.get(`/add`, (req, res) => res.send(`/offers/add`));
-router.get(`/edit/:id`, (req, res) => res.send(`/offers/edit/:id`));
-router.get(`/:id`, (req, res) => res.send(`/offers/:id`));
+router.get(`/category/:id`, (req, res) => res.render(`category`));
+router.get(`/add`, (req, res) => res.render(`new-ticket`));
+router.get(`/edit/:id`, (req, res) => res.render(`ticket-edit`));
+router.get(`/:id`, (req, res) => res.render(`ticket`));
 
 module.exports = router;
